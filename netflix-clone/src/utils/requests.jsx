80264c4d2d284318acef4f0725a0c362b421040b
abel <abelgebeyehub@gmail.com>
2024-11-20 11:32:09 -0500

    banner modified

--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,4 +1,4 @@
-const API_KEY = '199915e57036d50fc49b26bcd5e05e4f'
+const API_KEY = process.env.VITE_API_KEY;
 const requests = {
 fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
 };
