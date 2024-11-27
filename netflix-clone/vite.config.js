301fb82e48237fbb5a72f85d1cad11484110e6a9
abel <abelgebeyehub@gmail.com>
2024-11-27 10:48:32 -0500

    vite config correction

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
-import react from '@vitejs/plugin-react'
-//  plugins: [react()]
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    // ... other Vite config
-    plugins: [react()],
-    define: {
-      "process.env": env,
-      // base: "/Netflix-Clone-2024/netflix-clone/"
-    },
-  };
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+ base: "/Netflix-Clone-2024/"
 });
+
